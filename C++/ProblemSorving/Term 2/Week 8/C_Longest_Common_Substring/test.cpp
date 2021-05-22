ef147b45bace6e5c14a8b24bed68c5c8d7452594
Mr.White <15861602727@163.com>
2021-05-22 21:09:33 +0800

    add revision/other solutions for Prob-Sol T2W5-W6

--- C++/ProblemSorving/Term 2/Week 8/C_Longest_Common_Substring/test.cpp
+++ C++/ProblemSorving/Term 2/Week 8/C_Longest_Common_Substring/test.cpp
@@ -1,18 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
+string A, B, n, m;
 int main()
 {
-    string A, B;
     cin >> B >> A;
-    vector<int> pre(A.size() + 1), cur(A.size() + 1);
-
-    for (int j = 1; j <= B.size(); ++j){//in j iteration, cur[i] means i of A, j of B
-        for (int i = 1; i <= A.size(); ++i)
+    int n = A.size();
+    int m = B.size();
+    static vector<int> pre(n + 1), cur(n + 1);
+    for (int j = 1; j <= m; ++j){//in j iteration, cur[i] means i of A, j of B
+        for (int i = 1; i <= n; ++i)
             if (A[i - 1] == B[j - 1]) cur[i] = pre[i - 1] + 1;
             else cur[i] = max(pre[i], cur[i - 1]);
         pre.swap(cur);
     }
-    cout << pre[A.size()];
+    cout << pre[n];
 
     return 0;
 }
