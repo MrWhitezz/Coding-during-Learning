002f36394355c638f794ae45f01761091d833cfb
Stephen White <15861602727@163.com>
2021-05-29 23:59:46 +0800

    English Paper

--- C++/To learn/Others/xyq/paperAssess_Eng.cpp
+++ C++/To learn/Others/xyq/paperAssess_Eng.cpp
@@ -4,11 +4,12 @@ unordered_map<string, vector<double>> paper;
 vector<pair<double, string>> score;
 string name;
 double i = 0;
-const int N = 3;
+const int N = 20;
 void judge(ifstream &in);
 int main()
 {
     ifstream ifs;
+    ofstream ofs;
     for (int j = 1; j <= N; ++j){
         ifs.open(to_string(j) + ".txt" , ios:: in);
         if (!ifs.is_open()) cout << "Fail to open\n";
@@ -18,10 +19,14 @@ int main()
     for (auto it = paper.cbegin(); it != paper.cend(); ++it)
         score.push_back({accumulate(it->second.begin(), it->second.end(), 0.0) / it->second.size(), it->first});
     sort(score.begin(), score.end());
-    cout << "name\trank\tscore\n";
+    ofs.open("rank.txt");
+    ofs << "name\trank\tscore_ave\t\tscore1\tscore2\tscore3\tscore4\tscore5\tscore6\n";
     i = 1;
     for (auto it = score.crbegin(); it != score.crend(); ++it){
-        cout << it->second << '\t' << i++ << '\t' << it->first << endl;
+        ofs << it->second << '\t' << i++ << '\t' << it->first << "\t\t";
+        for (auto jt = paper[it->second].begin(); jt != paper[it->second].end(); ++jt)
+            ofs << *jt << '\t';
+        ofs << endl;
     }
 }
 void judge(ifstream &in){
